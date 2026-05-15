2c15aacb2d639bc9bf55a472c9add2131b9958c4
subhakantrout <subhakantrout@users.noreply.github.com>
2026-05-15 21:07:54 +0530

    Critical Fix: Path Resolution and Cache Busting

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/project-void/sw.js', { scope: '/project-void/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
